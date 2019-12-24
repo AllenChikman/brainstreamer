fd5781ec171b277c80f4209b65fc2dba98ab8209
Allen Chikman <allenchikman@gmail.com>
2019-12-24 22:22:11 +0200

    restructure

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
-
 setup(
-    name = 'foobar',
-    version = '0.1.0',
-    author = 'Dan Gittik',
-    description = 'An example package.',
-    packages = find_packages(),
-    install_requires = ['click', 'flask'],
-    tests_require = ['pytest', 'pytest-cov'],
+    name='brain-computer-interface',
+    version='0.1.0',
+    author='Allen Chikman',
+    description='Imaginary hardware , that can read minds, and upload snapshots of cognitions',
+    packages=find_packages(),
+    install_requires=['click', 'flask'],
+    tests_require=['pytest', 'pytest-cov'],
 )
---
+++ tests/first_test.py
@@ -0,0 +1,10 @@
+from bci import Thought
+
+
+def test_thought():
+    thought = Thought(3, 4, 5)
+    print(thought)
+
+
+def test_f():
+    pass

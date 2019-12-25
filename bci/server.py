6310eb7b717283033f13c6aeb90d059fa0ee7a5a
Allen Chikman <allenchikman@gmail.com>
2019-12-25 11:43:09 +0200

    trivial

---
+++ bci/server.py
@@ -0,0 +1,81 @@
+import os
+import threading
+import socket
+import time
+import struct
+from cli import CommandLineInterface
+
+cli = CommandLineInterface()
+
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def append_to_file(path, text):
+    f = open(path, "a")
+    f.write(text)
+    f.close()
+
+
+def _receive_all(connection, size):
+    chunks = []
+    while size > 0:
+        chunk = connection.recv(size)
+        if not chunk:
+            raise RuntimeError('incomplete data')
+        chunks.append(chunk)
+        size -= len(chunk)
+    return b''.join(chunks)
+
+
+class Handler(threading.Thread):
+    lock = threading.Lock()
+
+    def __init__(self, client, data_dir):
+        super().__init__()
+        self.client = client
+        self.data_dir = data_dir
+
+    def run(self):  # start invokes run
+
+        chunk = _receive_all(self.client, 20)
+        res = struct.unpack('QQi', chunk)
+
+        thought_len = int(res[2])
+        str_pack_format = str(thought_len) + 's'
+
+        chunk = _receive_all(self.client, thought_len)
+        thought = struct.unpack(str_pack_format, chunk)
+
+        formatted_date = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(res[1]))
+        thought_str = thought[0].decode()
+
+        dir_path = str(self.data_dir) + "/" + str(res[0])
+        file_path = dir_path + "/" + formatted_date + ".txt"
+        self.lock.acquire()
+        create_dir(dir_path)
+        if os.path.isfile(file_path):
+            append_to_file(file_path, "\n")
+        append_to_file(file_path, thought_str)
+        self.lock.release()
+
+
+@cli.command
+def run(address, data):
+    formatted_address = address.split(":")[0], int(address.split(":")[1])
+    server = socket.socket()
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(formatted_address)
+
+    while True:
+        server.listen(1000)
+        client, formatted_address = server.accept()
+
+        handler = Handler(client, data)
+        handler.start()
+
+
+if __name__ == '__main__':
+    cli.main()

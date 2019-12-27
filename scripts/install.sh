4a80f037968e34c5732e7f55d102e2e4a4f9a027
Allen Chikman <allenchikman@gmail.com>
2019-12-27 21:11:32 +0200

    trivial

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
-    python -m virtualenv .env --prompt "[foobar] "
+    python -m virtualenv .env --prompt "[brain-computer-interface] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
